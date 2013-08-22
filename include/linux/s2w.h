1be388f6a5a65ad3611ba96e2a2b0a3f19aae5bb
ayysir <dresadd09691@gmail.com>
2013-08-22 19:21:16 -0400

    s2w: Sweep 2 Wake
    [*]S2W: added kconfig definitions to s2w coded files
    [*]Added s2w to defconfig
    
    Enabled by default
    
    Referenced Ktoonsez & Showp1984's code & structure
    
    thank you guys

---
+++ include/linux/s2w.h
@@ -0,0 +1,5 @@
+extern void s2w_setdev(struct input_dev *input_device);
+extern void s2w_change(unsigned int val);
+extern bool s2w_enabled;
+extern bool s2w_enabled_plug;
+extern bool s2w_prox_near;
